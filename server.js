6a8b332650a16b4cc4959a7aece8dfeb9057244d
Annie Chiang <devbear22@gmail.com>
2017-10-25 14:28:33 -0700

    added export

--- server.js
+++ server.js
@@ -7,3 +7,9 @@ app.use(express.static('public'));
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
 });
+
+if (require.main === module) {
+  runServer().catch(err => console.error(err));
+};
+
+module.exports = {app};
