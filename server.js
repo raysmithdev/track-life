1fbeeb7bd2c0b727ee55821f78dee71d4550ba6b
Annie Chiang <devbear22@gmail.com>
2017-10-25 13:57:01 -0700

    updated initial deploy test

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 'use strict';
 
 const express = require('express');
-var app = express();
+const app = express();
 
 app.use(express.static('public'));
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080, () => {
+  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
+});
