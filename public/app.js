4edec5f75aa946c13bc4487736228ac958017c6f
Annie Chiang <devbear22@gmail.com>
2017-10-28 10:02:40 -0700

    add mock data

---
+++ public/app.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const mockData = [
+  {
+    id: 1,
+    name: 'Eat more tofu',
+    description: 'it\'s good for you, I think',
+    createdDate: '2017-10-22 20:38:43',
+    notes: 'tofu steak recipe http://www.justmoretofu.com/steak',
+    tallyMarks: {
+      '2017-08': 10,
+      '2017-09': 5,
+      '2017-10': 11,
+    }
+  },
+  {
+    id: 2,
+    name: 'Go to Jiu Jitsu',
+    description: 'get more flow',
+    createdDate: '2017-01-28 20:38:43',
+    notes: '10/27 - loop choke from guard',
+    tallyMarks: {
+      '2017-07': 9,
+      '2017-08': 8,
+      '2017-09': 12,
+      '2017-10': 11,
+    }
+  },
+  {
+    id: 3,
+    name: 'Do Back Exercises',
+    description: 'strength for bjj',
+    createdDate: '2017-01-28 20:38:43',
+    notes: 'Rener video',
+    tallyMarks: {
+      '2017-09': 3,
+      '2017-10': 4,
+    }
+  },
+];
