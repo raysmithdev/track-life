472bb7ab991ea070c8248ee531c19501b76f92b8
Annie Chiang <devbear22@gmail.com>
2017-11-05 20:26:57 -0800

    import model and add express router

---
+++ src/tracker/tracker.router.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const { Tracker } = require('./tracker.model');
+
+const router = express.Router(); 
---
+++ src/user/user.router.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const { User } = require('./user.model');
+
+const router = express.Router(); 
+
+
+
