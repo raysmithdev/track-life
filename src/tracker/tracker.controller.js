74eac693e4383cd48066a6a0bc0a936e66d91908
Annie Chiang <devbear22@gmail.com>
2017-11-05 21:44:13 -0800

    initial commit

---
+++ src/tracker/tracker.controller.js
@@ -0,0 +1,24 @@
+'use strict';
+
+//object? is this the correct filepath? 
+const {Tracker} = require('./tracker.model');
+
+//get all existing trackers from user (dashboard & summary display)
+
+//get single tracker from user (by id) - individual summary display
+
+//create a new tracker
+
+//add mark to a tracker (update by id)
+
+//remove a mark to a tracker (update by id)
+
+//modify description (update by id)
+
+//modify notes (update by id)
+
+//archive tracker (change status by id)
+
+//reactivate archived tracker (change status by id)
+
+//delete tracker 
---
+++ src/user/user.controller.js
@@ -0,0 +1,10 @@
+'use strict';
+
+//object? is this the correct filepath? 
+const {User} = require('./user.model');
+
+//create a new user
+
+//change user avatar
+
+//delete user
