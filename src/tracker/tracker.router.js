ca4908e4fffacc96ac13bd9fe39b7ce09b6b30b6
Annie Chiang <devbear22@gmail.com>
2017-11-06 21:09:18 -0800

    initial routes for get

--- src/tracker/tracker.router.js
+++ src/tracker/tracker.router.js
@@ -5,3 +5,14 @@ const bodyParser = require('body-parser');
 const {Tracker} = require('./tracker.model');
 
 const router = express.Router(); 
+
+
+router.get('/', );
+
+router.get('/trackers', )
+router.get('/trackers/:trackerId', ) //query with user id?
+
+router.get('/user', )
+router.get('/user/:userId', )
+router.get('/users/:userId/trackers', )
+router.get('/users/:userId/trackers/:trackerId', )
