eb98a1c7c5408feb5a645e726759bb5ae8e8715a
Annie Chiang <devbear22@gmail.com>
2017-11-06 21:09:42 -0800

    GET all trackers

--- src/tracker/tracker.controller.js
+++ src/tracker/tracker.controller.js
@@ -2,12 +2,35 @@
 
 //object? is this the correct filepath? 
 const {Tracker} = require('./tracker.model');
+const {User} = require('../user/user.model');
 
 //get all existing trackers from user (dashboard & summary display)
+const getAllTrackers = (req, res) => {
+  Tracker
+    .find()
+    .then(trackers => {
+      res.json({
+        trackers: trackers.map(trackers => trackers.toClient())
+      });
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ message: "internal server error"});
+    });
+};
 
 //get single tracker from user (by id) - individual summary display
 
-//create a new tracker
+//create a new tracker -- get user by id and add
+const createNewTracker = (req, res) => {
+  Tracker
+    .create({
+      name: req.body.name,
+      description: req.body.description,
+      notes: req.body.notes,
+    })
+    .then()
+}
 
 //add mark to a tracker (update by id)
 
