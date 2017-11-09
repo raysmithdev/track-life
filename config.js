c720c08ba15c3b9153d1b5a4799a1bb6ae3a1138
Annie Chiang <devbear22@gmail.com>
2017-11-09 13:38:45 -0800

    export config as module

--- config.js
+++ config.js
@@ -4,4 +4,6 @@ const DATABASE_URL = process.env.DATABASE_URL ||
                     global.DATABASE_URL ||
                     'mongodb://localhost/track-life';
 
-exports.PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
+
+module.exports = {DATABASE_URL, PORT};
