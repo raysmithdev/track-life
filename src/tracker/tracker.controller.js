d5f6df9a122c5201da6a53ad1c97112d8f830abc
Annie Chiang <devbear22@gmail.com>
2017-11-08 20:32:49 -0800

    create new tracker function

--- src/tracker/tracker.controller.js
+++ src/tracker/tracker.controller.js
@@ -1,11 +1,13 @@
 'use strict';
 
-//object? is this the correct filepath? 
 const {Tracker} = require('./tracker.model');
 const {User} = require('../user/user.model');
+const ObjectId = require('mongodb');
+const TrackerStatuses = require('./tracker-status.enum');
 
 //get all existing trackers from user (dashboard & summary display)
-const getAllTrackers = (req, res) => {
+const findAllTrackers = (req, res) => {
+  
   Tracker
     .find()
     .then(trackers => {
@@ -19,7 +21,38 @@ const getAllTrackers = (req, res) => {
     });
 };
 
-//get single tracker from user (by id) - individual summary display
+// tracker code needs to go into that then of the User.findById statement.  
+// don't set the res.json yet because you have to finish your logic 
+// and then set it when you're done, you can't send the response twice
+
+// user.trackerIds.find((tracker) => tracker._id === req.params.trackerId)
+
+//get single tracker -- query the tracker then query the user 
+const findOneTracker = (req, res) => {
+  const trackerId = req.params.trackerId; //is this correct?
+  const userId= req.params.userId;
+  // const userTrackers = req.
+  //return the array of trackerIds from user model -- look up the code paths 
+  //linking 2 models together 
+  User
+    .findById(userId)
+    //.populate(user.trackerIds)
+    .then(user => {
+      const foundId = user.trackerIds.find(id => id.equals(new ObjectId(trackerId)));
+      //res.json(user.toClient());
+    })
+    console.log(user);
+
+  Tracker
+    .findById(trackerId)
+    .then(tracker => {
+      res.json(tracker.toClient());
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ message: "internal server error"});        
+    });
+}
 
 //create a new tracker -- get user by id and add
 const createNewTracker = (req, res) => {
@@ -27,10 +60,19 @@ const createNewTracker = (req, res) => {
     .create({
       name: req.body.name,
       description: req.body.description,
+      status: TrackerStatuses.ACTIVE, //?
+      createdDate: new Date(),
       notes: req.body.notes,
+      userId: req.params.userId //how to handle this? 
     })
-    .then()
-}
+    .then(tracker => {
+      res.status(201).json(tracker.toClient())
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ message: "internal server error"});        
+    });
+};
 
 //add mark to a tracker (update by id)
 
@@ -41,7 +83,11 @@ const createNewTracker = (req, res) => {
 //modify notes (update by id)
 
 //archive tracker (change status by id)
-
+const archiveTracker = (req, res) => {
+  trackerId = 
+  Tracker
+    .findByIdAndUpdate()
+}
 //reactivate archived tracker (change status by id)
 
 //delete tracker 
