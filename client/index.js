8ca1435a0b2072b1105e8519b988964594fd6e39
Annie Chiang <devbear22@gmail.com>
2017-11-09 15:41:30 -0800

    add dashboardtracker() to query existing trackers & move to local server data

--- client/index.js
+++ client/index.js
@@ -4,6 +4,7 @@ import ChartComponents from "./chart.component";
 import TrackerComponents from "./tracker.component";
 
 import mockTrackerData from "./mock-data";
+import { debug } from "util";
 
 const STATE = {
   trackers: []
@@ -17,6 +18,7 @@ const STATE = {
 //pass in mockdata in renderDashboard as a test later
 // render dashboard
 function renderDashboard() {
+  console.log('render dashboard');
   $(".main-section").hide();
   $(".dashboard-container").empty(); //html('');
   // $('.tracker-container').empty();
@@ -51,6 +53,16 @@ function renderSummaryPage() {
 
   $(".tracker-summary").show();
 }
+
+// function toggleChartType() {
+
+//   let trackerData = STATE.trackers.find(tracker => tracker.id === id);
+//   const trackerComponent = new TrackerComponents(trackerData);
+//   $(".summary-container").append(trackerComponent.getTrackerSummaryHtml());
+
+//   const chartComponent = new ChartComponents(trackerData);
+//   chartComponent.renderChart();
+// }  
 
 //render individual tracker summary
 function renderIndividualTrackerSummary(id) {
@@ -96,8 +108,7 @@ function renderLogOutDashboard() {
 
 //add a mark to an existing tracker
 function addMarkToTracker() {
-  //check if current month exists & increment by 1
-  //if current month does not exist, add new and increment by 1
+  
 }
 
 function setUpHandlers() {
@@ -110,6 +121,9 @@ function setUpHandlers() {
   $(".logout-btn").click(renderLogOutDashboard);
 
   //dyanmic buttons created within trackers
+  //add mark button
+  //add delete button
+  //add archive button
 
   //view summary button
   $(".dashboard").on("click", ".view-sumry-btn", e => {
@@ -118,25 +132,33 @@ function setUpHandlers() {
     renderIndividualTrackerSummary(trackerId);
   });
 
-  //add mark button
-  //add delete button
-  //add archive button
+  //toggle chart type - line <> bar graph
+  // $(".tracker-summary").on("click", ".toggle-chart", e => {
+  //   toggleChartType();
+  // })
+
 
   //add close button - close individual summary & back to summary page
   $(".tracker-summary").on("click", ".close-btn", () => {
     renderSummaryPage();
     console.log('close');
   });
-
+  
 }
 
 function getDashboardTrackers() {
   // this will call the api for trackers and store in STATE
-  STATE.trackers.push(...mockTrackerData);
+  
+  // TODO: update the 123 to be an id when we are ready
+  return $.get('/api/users/123/trackers').then(data => {
+    console.log(data.trackers);
+    // STATE.trackers.push(...mockTrackerData);
+    STATE.trackers.push(...data.trackers);
+  });
 }
 
 $("document").ready(() => {
   setUpHandlers();
-  getDashboardTrackers()
-  renderDashboard(); //this should run after user logs in
+  //this should run after user logs in
+  getDashboardTrackers().then(renderDashboard)
 });
