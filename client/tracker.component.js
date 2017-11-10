e68941b300ece2d3ce03d2660f2da32d19dbb8f8
Annie Chiang <devbear22@gmail.com>
2017-11-10 14:03:23 -0800

    test render description input in indivdiual summary view // remove for now

--- client/tracker.component.js
+++ client/tracker.component.js
@@ -87,7 +87,7 @@ export default class TrackerComponents {
   // }
 
   //marks are rendering outside of container?
-  getTrackerHtml() {
+  getDashboardTrackerHtml() {
     const template = `
       <div class="tracker-container dash-trkr-box">
         <h3 class="tracker-name">${this.name}</h3>
@@ -123,7 +123,6 @@ export default class TrackerComponents {
       </div>
       <div class="col-2">
         <div class="chart-container">
-          <button type="button" class="toggle-chart chart-btn">Toggle</button>
           <canvas class="myChart-${this.trackerId}"></canvas>
         </div>
         <div class="notes-container">
@@ -142,36 +141,6 @@ export default class TrackerComponents {
     return template;
   }
 
-  // getTrackerSummaryHtml() {
-  //   const template = `
-  //     <div class="tracker-container inner-flexbox">
-  //     <div class="col-1">
-  //       <h3 class="tracker-name">${this.name}</h3>
-  //       <h4 class="tracker-month">${this.currentMarks.currentTrackerMonth}</h4>
-  //         <div class="marks-container">
-  //           <ul class="tally-marks>${this.getTallyMarks()}</ul> 
-  //         </div>
-  //         <div class="summary-statements">
-  //           <p class="summary-sentence">You marked ${this.name} ${this.currentMarks.monthCount} times this month!</p>
-  //           <p class="summary-sentence">You marked ${this.name} ${this.oneMonthBack.monthCount} times last month!</p>
-  //         </div>
-  //     </div>
-  //     <div class="col-2">
-  //       <div class="chart-container">
-  //         <canvas class="myChart-${this.trackerId}"></canvas>
-  //       </div>
-  //       <div class="summary-btn-row">
-  //         <button type="button" class="edit-trkr-btn trkr-btn">Edit</button>        
-  //         <button type="button" class="add-mark-btn trkr-btn">Add Mark</button>
-  //         <button type="button" class="delete-btn trkr-btn">Delete</button> 
-  //         <button type="button" class="archive-btn trkr-btn">Archive</button>
-  //       </div>
-  //     </div>
-  //   </div>
-  //   `;
-  //   return template;
-  // }
-
   getIndividualTrackerHtml() {
     const template = `
       <div class="tracker-container inner-flexbox">
@@ -179,14 +148,14 @@ export default class TrackerComponents {
         <h3 class="tracker-name">${this.name}</h3>
         <h4 class="tracker-month">${this.currentMarks.currentTrackerMonth}</h4>
         <p class="description">${this.description}</p>
-          <div class="marks-container">
-            <ul class="tally-marks>${this.getTallyMarks()}</ul> 
-          </div>
-          <div class="summary-statements">
-            <p class="summary-sentence">You marked ${this.name} ${this.currentMarks.monthCount} times this month!</p>
-            <p class="summary-sentence">You marked ${this.name} ${this.oneMonthBack.monthCount} times last month!</p>
-            <p class="summary-sentence"> On average, you mark ${this.name} ${/*this.averageMarks*/''} </p>             
-          </div>
+        <div class="marks-container">
+          <ul class="tally-marks>${this.getTallyMarks()}</ul> 
+        </div>
+        <div class="summary-statements">
+          <p class="summary-sentence">You marked ${this.name} ${this.currentMarks.monthCount} times this month!</p>
+          <p class="summary-sentence">You marked ${this.name} ${this.oneMonthBack.monthCount} times last month!</p>
+          <p class="summary-sentence"> On average, you mark ${this.name} ${/*this.averageMarks*/''} </p>             
+        </div>
       </div>
       <div class="col-2">
         <div class="chart-container">
@@ -197,10 +166,10 @@ export default class TrackerComponents {
           <textarea class="trkr-sumry-notes">${this.notes}</textarea>
         </div>
         <div class="summary-btn-row">
-          <button type="button" data-trkr-id=${this.trackerId} class="edit-trkr-btn trkr-btn">Edit</button>
+          <button type="button" data-trkr-id=${this.trackerId} class="save-btn trkr-btn">Save</button>
           <button type="button" data-section="single" data-trkr-id=${this.trackerId} class="add-mark-btn trkr-btn">Add Mark</button>
           <button type="button" data-trkr-id=${this.trackerId} class="delete-btn trkr-btn">Delete</button> 
-          <button type="button" class="archive-btn trkr-btn">Archive</button>
+          <button type="button" data-trkr-id=${this.trackerId} class="archive-btn trkr-btn">Archive</button>
           <button type="button" class="close-btn trkr-btn">Close</button>
         </div>
       </div>
@@ -237,3 +206,9 @@ export default class TrackerComponents {
     return template;
   }
 }
+
+//decide whether or not to add description box in individual tracker summary html
+//<div class="description-container">
+  //<label for="existing-tracker-description">Description</label>
+  //<input class="existing-trkr-desc" type="text"></input>
+//</div>
