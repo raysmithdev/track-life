95b0bf12127b36d47939edcdc281eea59f2b3ecf
Annie Chiang <devbear22@gmail.com>
2017-11-10 08:56:56 -0800

    change add mark to post vs put

--- src/tracker/tracker.router.js
+++ src/tracker/tracker.router.js
@@ -8,8 +8,6 @@ const router = express.Router();
 
 router.use(bodyParser.json());
 //router.get('/', );
-// router.get('/trackers', )
-// router.get('/trackers/:trackerId', ) //query with user id?
 
 //get all trackers from user
 router.get('/users/:userId/trackers', findAllTrackers)
@@ -17,20 +15,21 @@ router.get('/users/:userId/trackers', findAllTrackers)
 //create new tracker
 router.post('/users/:userId/trackers', createNewTracker);
 
-//add mark - increment by 1
-//router.post('/users/:userId/trackers/:trackerId', addMark)
-
-//modify tracker to add a mark to it
-//how to attach this to Add Mark button?
-router.put('/users/:userId/trackers/:trackerId', addMark)
+//add mark to an existing tracker (increment by 1)
+router.post('/users/:userId/trackers/:trackerId', addMark)
 
 //modify tracker details (name, description, notes)
-router.put('/users/:userId/trackers/:trackerId', )
+// router.put('/users/:userId/trackers/:trackerId', )
 
 //archive a tracker
 //router.put('/users/:userId/trackers/:trackerId', )
 
+module.exports = router;
+
+//extra routes for now 
+
 //access individual tracker -- modify as needed
 //router.get('/users/:userId/trackers/:trackerId',)
 
-module.exports = router;
+// router.get('/trackers', )
+// router.get('/trackers/:trackerId', ) //query with user id?
