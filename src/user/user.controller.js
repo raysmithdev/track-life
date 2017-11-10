b9a81a4ef5cb73147ffee6053463b00ee90aa299
Annie Chiang <devbear22@gmail.com>
2017-11-09 20:58:58 -0800

    add user routes for create new user &  get all users

--- src/user/user.controller.js
+++ src/user/user.controller.js
@@ -1,10 +1,49 @@
 'use strict';
 
 //object? is this the correct filepath? 
-const {User} = require('./user.model');
+const { User } = require('./user.model');
+const ObjectId = require('mongodb');
+
+// get all users
+const getAllUsers = (req, res) => {
+  User
+    .find()
+    .then(users => {
+      res.json({
+        users: users.map(users => users.toClient())
+      });
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ message: "internal server error"});
+    });
+};
 
 //create a new user
+const createNewUser = (req, res) => {
+  User
+    .create({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      userName: req.body.userName,
+      password: req.body.password,
+      avatar: req.body.avator,
+      trackerIds: req.body.trackerIds
+    })
+    .then(user => {
+      res.status(201).json(user.toClient())
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ message: "internal server error"});        
+    });
+};
 
 //change user avatar
 
 //delete user
+
+module.exports = {
+  getAllUsers,
+  createNewUser
+}
--- src/user/user.router.js
+++ src/user/user.router.js
@@ -2,10 +2,21 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const { User } = require('./user.model');
-
+const { getAllUsers, createNewUser } = require('./user.controller');
 const router = express.Router(); 
 
+router.use(bodyParser.json());
 
+//get all users
+router.get('/user', getAllUsers);
+
+//create new user 
+router.post('/user', createNewUser);
+
+//update user
+// router.put('/user/:userId,')
+
+//delete user 
+//router.get('/user/:userId', )
 
