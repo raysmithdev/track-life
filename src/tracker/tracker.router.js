b6ec68f73e647c7187cdc7dc63cd3c2abeda1510
Annie Chiang <devbear22@gmail.com>
2017-11-09 20:59:29 -0800

    add mark route

--- src/tracker/tracker.router.js
+++ src/tracker/tracker.router.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 //const {Tracker} = require('./tracker.model');
-const {addMark, createNewTracker, findAllTrackers} = require('./tracker.controller');
+const { addMark, createNewTracker, findAllTrackers } = require('./tracker.controller');
 const router = express.Router(); 
 
 router.use(bodyParser.json());
@@ -12,25 +12,23 @@ router.use(bodyParser.json());
 // router.get('/trackers', )
 // router.get('/trackers/:trackerId', ) //query with user id?
 
-//router.get('/user', )
-//router.get('/user/:userId', )
-
 //get all trackers from user
 router.get('/users/:userId/trackers', findAllTrackers)
-
-//access individual tracker -- modify as needed
-//router.get('/users/:userId/trackers/:trackerId',)
 
 //create new tracker
 router.post('/users/:userId/trackers', createNewTracker);
 
 //add mark - increment by 1
-router.post('/users/:userId/trackers', addMark)
+// router.post('/users/:userId/trackers/:trackerId', addMark)
 
 //modify tracker to add a mark to it
 //how to attach this to Add Mark button?
-router.put('/users/:userId/trackers', addMark)
+router.put('/users/:userId/trackers/:trackerId', addMark)
+
 //archive a tracker
 //router.put('/users/:userId/trackers/:trackerId', )
+
+//access individual tracker -- modify as needed
+//router.get('/users/:userId/trackers/:trackerId',)
 
 module.exports = router;
