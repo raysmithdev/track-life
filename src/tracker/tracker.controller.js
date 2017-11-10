f440f8e3813cb370cd323b9568830f9089600cf1
Annie Chiang <devbear22@gmail.com>
2017-11-09 16:53:38 -0800

    create new tracker handler - check month exist or not

--- src/tracker/tracker.controller.js
+++ src/tracker/tracker.controller.js
@@ -1,11 +1,13 @@
 'use strict';
 
-const {Tracker} = require('./tracker.model');
-const {User} = require('../user/user.model');
+const { Tracker } = require('./tracker.model');
+const { User } = require('../user/user.model');
 const ObjectId = require('mongodb');
+const moment = require('moment');
 const TrackerStatuses = require('./tracker-status.enum');
 
-//get all existing trackers from user (dashboard & summary display)
+//get all existing trackers from user
+// (dashboard & summary display)
 const findAllTrackers = (req, res) => {
   
   Tracker
@@ -21,6 +23,82 @@ const findAllTrackers = (req, res) => {
     });
 };
 
+//create a new tracker -- get user by id and add
+const createNewTracker = (req, res) => {
+  const tallyMarks = {};
+  tallyMarks[moment().format('YYYY-MM-01')] = 1;
+  Tracker
+    .create({
+      name: req.body.name,
+      description: req.body.description,
+      status: TrackerStatuses.ACTIVE, 
+      createdDate: new Date(),
+      notes: req.body.notes,
+      userId: req.params.userId, //how to handle this?
+      tallyMarks
+    })
+    .then(tracker => {
+      res.status(201).json(tracker.toClient())
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ message: "internal server error"});        
+    });
+};
+
+//add mark to a tracker (update by id)
+const addMark = (req, res) => {
+  Tracker
+    //query for tracker by id
+    .findById(req.params.trackerId)
+    //check if it's the current month, then increment by 1
+    .then(tracker => { 
+      const currentMonth = moment();
+      const sortedKeys = Object.keys(tracker.tallyMarks).sort();
+      const trackerMonth = sortedKeys[sortedKeys.length - 1];
+      const trackerMoment = moment(trackerMonth);
+  
+      const doesCurrentMonthMatch = trackerMoment.isSame(currentMonth, "month");
+      if (doesCurrentMonthMatch === true) {
+        tracker.tallyMarks[trackerMonth]++
+      } else {
+      //if it is not current month, add new month & add 1 mark
+        tracker.tallyMarks[currentMonth.format('YYYY-MM-01')] = 1;
+      }
+    tracker.save(); 
+    })
+    .then(tracker => {
+      //return the whole tracker 
+      res.status(201).json(tracker.toClient())
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ message: "internal server error"});        
+    });
+}
+//remove a mark to a tracker (update by id)
+
+//modify description (update by id)
+
+//modify notes (update by id)
+
+//archive tracker (change status by id)
+const archiveTracker = (req, res) => {
+  const trackerId = '';
+  Tracker
+    .findByIdAndUpdate()
+}
+//reactivate archived tracker (change status by id)
+
+//delete tracker 
+
+module.exports = {
+  createNewTracker,
+  findAllTrackers,
+  addMark
+};
+
+
 // tracker code needs to go into that then of the User.findById statement.  
 // don't set the res.json yet because you have to finish your logic 
 // and then set it when you're done, you can't send the response twice
@@ -28,7 +106,7 @@ const findAllTrackers = (req, res) => {
 // user.trackerIds.find((tracker) => tracker._id === req.params.trackerId)
 
 //get single tracker -- query the tracker then query the user 
-const findOneTracker = (req, res) => {
+/*const findOneTracker = (req, res) => {
   const trackerId = req.params.trackerId; //is this correct?
   const userId= req.params.userId;
   // const userTrackers = req.
@@ -52,42 +130,4 @@ const findOneTracker = (req, res) => {
       console.error(err);
       res.status(500).json({ message: "internal server error"});        
     });
-}
-
-//create a new tracker -- get user by id and add
-const createNewTracker = (req, res) => {
-  Tracker
-    .create({
-      name: req.body.name,
-      description: req.body.description,
-      status: TrackerStatuses.ACTIVE, //?
-      createdDate: new Date(),
-      notes: req.body.notes,
-      userId: req.params.userId //how to handle this? 
-    })
-    .then(tracker => {
-      res.status(201).json(tracker.toClient())
-    })
-    .catch(err => {
-      console.error(err);
-      res.status(500).json({ message: "internal server error"});        
-    });
-};
-
-//add mark to a tracker (update by id)
-
-//remove a mark to a tracker (update by id)
-
-//modify description (update by id)
-
-//modify notes (update by id)
-
-//archive tracker (change status by id)
-const archiveTracker = (req, res) => {
-  trackerId = 
-  Tracker
-    .findByIdAndUpdate()
-}
-//reactivate archived tracker (change status by id)
-
-//delete tracker 
+}*/
