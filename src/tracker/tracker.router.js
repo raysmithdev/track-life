5351ee805cc7e1684e5148f4e447f359d5ade8c4
Annie Chiang <devbear22@gmail.com>
2017-11-09 16:54:17 -0800

    use GET/POST routes to get local server started by creating a tracker

--- src/tracker/tracker.router.js
+++ src/tracker/tracker.router.js
@@ -2,26 +2,35 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const {Tracker} = require('./tracker.model');
-
+//const {Tracker} = require('./tracker.model');
+const {addMark, createNewTracker, findAllTrackers} = require('./tracker.controller');
 const router = express.Router(); 
 
-
-router.get('/', );
+router.use(bodyParser.json());
+//router.get('/', );
 
 // router.get('/trackers', )
 // router.get('/trackers/:trackerId', ) //query with user id?
 
-router.get('/user', )
-
-router.get('/user/:userId', )
+//router.get('/user', )
+//router.get('/user/:userId', )
 
 //get all trackers from user
-router.get('/users/:userId/trackers', )
+router.get('/users/:userId/trackers', findAllTrackers)
 
 //access individual tracker -- modify as needed
-router.get('/users/:userId/trackers/:trackerId', )
+//router.get('/users/:userId/trackers/:trackerId',)
 
 //create new tracker
-router.post('/users/:userId/trackers', )
+router.post('/users/:userId/trackers', createNewTracker);
 
+//add mark - increment by 1
+router.post('/users/:userId/trackers', addMark)
+
+//modify tracker to add a mark to it
+//how to attach this to Add Mark button?
+router.put('/users/:userId/trackers', addMark)
+//archive a tracker
+//router.put('/users/:userId/trackers/:trackerId', )
+
+module.exports = router;
