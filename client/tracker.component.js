4553fbea9acdfc528561dc8fabe1dd70574ec833
Annie Chiang <devbear22@gmail.com>
2017-11-12 21:50:51 -0800

    prettify

--- client/tracker.component.js
+++ client/tracker.component.js
@@ -9,7 +9,7 @@ export default class TrackerComponents {
     this.notes = data.notes;
     this.tallyMarks = data.tallyMarks;
     this.currentMarks = this.checkTrackerMonth();
-    this.oneMonthBack = this.getPreviousCount(); 
+    this.oneMonthBack = this.getPreviousCount();
     // this.averageMarks = this.calculateAverage();
   }
 
@@ -44,7 +44,7 @@ export default class TrackerComponents {
     const isItMonthBefore = trackerMoment.isBefore(currentMonth, "month");
     if (isItMonthBefore === true) {
       return { monthCount: this.tallyMarks[previousMonth] };
-    } else { 
+    } else {
       return { monthCount: 0 };
     }
   }
@@ -65,7 +65,7 @@ export default class TrackerComponents {
   //   let totalMarks = 0;
   //   let avgMarks = 0;
   //   //add sum of last 6 months marks
-  //   //divide sum by 6 or max length? 
+  //   //divide sum by 6 or max length?
   //   for (let i = 0; i <= this.sortedKeys.length; i++) {
   //     // console.log(this.sortedKeys);
   //     totalMarks += this.tallyMarks[sortedKeys[i]];
@@ -87,9 +87,12 @@ export default class TrackerComponents {
               <ul class="tally-marks>${this.getTallyMarks()}</ul> 
             </div> 
           <div class="dashboard-btn-row">
-            <button type="button" data-section="dashboard" data-trkr-id=${this.trackerId} class="add-mark-btn trkr-btn">Add Mark</button>
-            <button type="button" data-section="dashboard" data-trkr-id=${this.trackerId} class="remove-mark-btn trkr-btn">Remove Mark</button>            
-            <button type="button" data-trkr-id=${this.trackerId} class="view-sumry-btn trkr-btn">View</button>
+            <button type="button" data-section="dashboard" data-trkr-id=${this
+              .trackerId} class="add-mark-btn trkr-btn">Add Mark</button>
+            <button type="button" data-section="dashboard" data-trkr-id=${this
+              .trackerId} class="remove-mark-btn trkr-btn">Remove Mark</button>            
+            <button type="button" data-trkr-id=${this
+              .trackerId} class="view-sumry-btn trkr-btn">View</button>
           </div>
       </div>
       `;
@@ -107,9 +110,12 @@ export default class TrackerComponents {
             <ul class="tally-marks>${this.getTallyMarks()}</ul> 
           </div>
           <div class="summary-statements">
-            <p class="summary-sentence">You marked ${this.name} ${this.currentMarks.monthCount} times this month!</p>
-            <p class="summary-sentence">You marked ${this.name} ${this.oneMonthBack.monthCount} times last month!</p>
-            <p class="summary-sentence"> On average, you mark ${this.name} ${/*this.averageMarks*/''} </p> 
+            <p class="summary-sentence">You marked ${this.name} ${this
+      .currentMarks.monthCount} times this month!</p>
+            <p class="summary-sentence">You marked ${this.name} ${this
+      .oneMonthBack.monthCount} times last month!</p>
+            <p class="summary-sentence"> On average, you mark ${this
+              .name} ${/*this.averageMarks*/ ""} </p> 
           </div>
       </div>
       <div class="col-2">
@@ -121,11 +127,16 @@ export default class TrackerComponents {
           <textarea class="trkr-sumry-notes">${this.notes}</textarea>
         </div>
         <div class="summary-btn-row">
-          <button type="button" data-trkr-id=${this.trackerId} class="edit-trkr-btn trkr-btn">Edit</button>
-          <button type="button" data-section="summary" data-trkr-id=${this.trackerId} class="add-mark-btn trkr-btn">Add Mark</button>
-          <button type="button" data-section="summary" data-trkr-id=${this.trackerId} class="remove-mark-btn trkr-btn">Remove Mark</button>                      
-          <button type="button" data-section="summary" data-trkr-id=${this.trackerId} class="delete-btn trkr-btn">Delete</button> 
-          <button type="button" data-section="summary" data-trkr-id=${this.trackerId} class="archive-btn trkr-btn">Archive</button>
+          <button type="button" data-trkr-id=${this
+            .trackerId} class="edit-trkr-btn trkr-btn">Edit</button>
+          <button type="button" data-section="summary" data-trkr-id=${this
+            .trackerId} class="add-mark-btn trkr-btn">Add Mark</button>
+          <button type="button" data-section="summary" data-trkr-id=${this
+            .trackerId} class="remove-mark-btn trkr-btn">Remove Mark</button>                      
+          <button type="button" data-section="summary" data-trkr-id=${this
+            .trackerId} class="delete-btn trkr-btn">Delete</button> 
+          <button type="button" data-section="summary" data-trkr-id=${this
+            .trackerId} class="archive-btn trkr-btn">Archive</button>
         </div>
       </div>
     </div>
@@ -144,9 +155,12 @@ export default class TrackerComponents {
           <ul class="tally-marks>${this.getTallyMarks()}</ul> 
         </div>
         <div class="summary-statements">
-          <p class="summary-sentence">You marked ${this.name} ${this.currentMarks.monthCount} times this month!</p>
-          <p class="summary-sentence">You marked ${this.name} ${this.oneMonthBack.monthCount} times last month!</p>
-          <p class="summary-sentence"> On average, you mark ${this.name} ${/*this.averageMarks*/''} </p>             
+          <p class="summary-sentence">You marked ${this.name} ${this
+      .currentMarks.monthCount} times this month!</p>
+          <p class="summary-sentence">You marked ${this.name} ${this
+      .oneMonthBack.monthCount} times last month!</p>
+          <p class="summary-sentence"> On average, you mark ${this
+            .name} ${/*this.averageMarks*/ ""} </p>             
         </div>
       </div>
       <div class="col-2">
@@ -158,11 +172,16 @@ export default class TrackerComponents {
           <textarea class="trkr-sumry-notes">${this.notes}</textarea>
         </div>
         <div class="summary-btn-row">
-          <button type="button" data-trkr-id=${this.trackerId} class="save-btn trkr-btn">Save</button>
-          <button type="button" data-section="single" data-trkr-id=${this.trackerId} class="add-mark-btn trkr-btn">Add Mark</button>
-          <button type="button" data-section="single" data-trkr-id=${this.trackerId} class="remove-mark-btn trkr-btn">Remove Mark</button>                      
-          <button type="button" data-section="single" data-trkr-id=${this.trackerId} class="delete-btn trkr-btn">Delete</button> 
-          <button type="button" data-section="single" data-trkr-id=${this.trackerId} class="archive-btn trkr-btn">Archive</button>
+          <button type="button" data-trkr-id=${this
+            .trackerId} class="save-btn trkr-btn">Save</button>
+          <button type="button" data-section="single" data-trkr-id=${this
+            .trackerId} class="add-mark-btn trkr-btn">Add Mark</button>
+          <button type="button" data-section="single" data-trkr-id=${this
+            .trackerId} class="remove-mark-btn trkr-btn">Remove Mark</button>                      
+          <button type="button" data-section="single" data-trkr-id=${this
+            .trackerId} class="delete-btn trkr-btn">Delete</button> 
+          <button type="button" data-section="single" data-trkr-id=${this
+            .trackerId} class="archive-btn trkr-btn">Archive</button>
           <button type="button" class="close-btn trkr-btn">Close</button>
         </div>
       </div>
@@ -191,7 +210,8 @@ export default class TrackerComponents {
           </div>
         <div class="summary-btn-row">
           <button type="button" class="delete-btn trkr-btn">Delete</button> 
-          <button type="button" data-trkr-id=${this.trackerId} class="reactivate-btn trkr-btn">Reactivate</button>
+          <button type="button" data-trkr-id=${this
+            .trackerId} class="reactivate-btn trkr-btn">Reactivate</button>
         </div>
       </div>
     </div>
@@ -202,6 +222,6 @@ export default class TrackerComponents {
 
 //decide whether or not to add description box in individual tracker summary html
 //<div class="description-container">
-  //<label for="existing-tracker-description">Description</label>
-  //<input class="existing-trkr-desc" type="text"></input>
+//<label for="existing-tracker-description">Description</label>
+//<input class="existing-trkr-desc" type="text"></input>
 //</div>
