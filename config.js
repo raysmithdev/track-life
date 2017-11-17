ad34d55ff42f08ec5927461e70be607e32a21019
Annie Chiang <devbear22@gmail.com>
2017-11-17 08:22:11 -0800

    initial setup for user authentication

--- config.js
+++ config.js
@@ -5,5 +5,7 @@ const DATABASE_URL = process.env.DATABASE_URL ||
                     'mongodb://localhost/track-life';
 
 const PORT = process.env.PORT || 8080;
+const JWT_SECRET = 'trackyolife';
+const JWT_EXPIRY = process.env.JWT_EXPIRY || '1d';
 
-module.exports = {DATABASE_URL, PORT};
+module.exports = {DATABASE_URL, PORT, JWT_EXPIRY, JWT_SECRET};
--- src/user/user.model.js
+++ src/user/user.model.js
@@ -1,5 +1,5 @@
 'use strict';
-
+const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema ({
@@ -28,6 +28,14 @@ userSchema.methods.toClient = function () {
     trackerIds: this.trackerIds
   };
 }
+
+userSchema.methods.validatePassword = function(password) {
+  return bcrypt.compare(password, this.password);
+};
+
+userSchema.statics.hashPassword = function(password) {
+  return bcrypt.hash(password, 10);
+};
 
 const User = mongoose.model('User', userSchema);
 
