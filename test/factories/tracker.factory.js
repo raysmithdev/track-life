9b546e7aaca70073d68d2b69fabe642f6aff4361
Annie Chiang <devbear22@gmail.com>
2017-11-21 12:53:34 -0800

    format document

--- test/factories/tracker.factory.js
+++ test/factories/tracker.factory.js
@@ -1,33 +1,35 @@
-const faker = require('faker');
-const { Tracker } = require('../../src/tracker/tracker.model');
-const moment = require('moment');
+const faker = require("faker");
+const { Tracker } = require("../../src/tracker/tracker.model");
+const moment = require("moment");
 
 function createOne(userId) {
   let tallyMarks = {};
   for (i = 0; i < 6; i++) {
     //get 6 random dates
-    let date =  faker.date.between(moment().subtract(faker.random.number({min:1, max:7}), 'months'), moment());
-    //reassigning to date as formatted 
+    let date = faker.date.between(
+      moment().subtract(faker.random.number({ min: 1, max: 7 }), "months"),
+      moment()
+    );
+    //reassigning to date as formatted
     date = moment(date).format("YYYY-MM-01");
     //randomly assign tally marks between 1-25
-    tallyMarks[date] = faker.random.number({min:1, max:25}); 
+    tallyMarks[date] = faker.random.number({ min: 1, max: 25 });
   }
 
   return {
     //userId: //add this back when user is created with parameter
-    name: faker.internet.domainName(),
+    name: faker.commerce.productName(),
     description: faker.company.catchPhraseDescriptor(),
-    status: faker.random.number({min: 1, max: 2}), 
+    status: faker.random.number({ min: 1, max: 2 }),
     notes: faker.company.catchPhrase(),
-    tallyMarks  //see if this compiles without needing tallyMarks key
+    tallyMarks
   };
 }
 
-function createMany (num) {
+function createMany(num) {
   let trackers = [];
-
   for (i = 0; i < num; i++) {
-    trackers.push(createOne()); 
+    trackers.push(createOne());
   }
   return trackers;
 }
