5fb66e89ba92587fc7ec93be1dc65fe32eeb6610
Annie Chiang <devbear22@gmail.com>
2017-11-21 20:43:37 -0800

    setup unit testing for endpoints

--- test/test-tracker.js
+++ test/test-tracker.js
@@ -7,6 +7,7 @@ const faker = require('faker');
 const mongoose = require('mongoose');
 const { app, runServer, closeServer } = require('../server');
 const { TEST_DATABASE_URL } = require('../config');
+
 const trackerFactory = require('../test/factories/tracker.factory');
 
 function tearDownDb() {
@@ -18,17 +19,12 @@ function tearDownDb() {
   });
 }
 
-describe('trackers', function() {
-  //let mockUser; 
+describe('tracker api', function() {
+  //let mockUser - need to incorporate user; 
 
   // start the server before test
   before(function() {
     return runServer(TEST_DATABASE_URL);
-  });
-
-  // close the server after the test is done
-  after(function() {
-    return closeServer();
   });
 
   beforeEach(function() {
@@ -41,18 +37,52 @@ describe('trackers', function() {
     return tearDownDb();
   });
 
-  //descirbe is a test case 
-  //it is one test case 
+  // close the server after the test is done
+  after(function() {
+    return closeServer();
+  });
 
-  describe('trackers-endpoints', function() {
-    
-    })
-})
+  describe('GET endpoint', function() { 
+    it('should return all existing trackers', function() {
+      
+    });
+    it('should return all active trackers', function() {
+      
+    });
+    it('should return all archived trackers', function() {
+      
+    });
+  });
+
+  describe('POST endpoint', function() { 
+    it('be able to add one mark to a tracker', function() {
+      
+    });
+    it('be able to remove one mark to a tracker', function() {
+      
+    });
+    it('change tracker status to archived', function() {
+
+    });
+    it('change tracker status to active (reactivated)', function() {
+      
+    });
+  });
+
+  describe('PUT endpoint', function() {
+    it('modify tracker fields - name, description, and notes', function() {
+      
+    });
+  });
+
+  describe('DELETE endpoint', function() {
+    //not sure if this will happen or not yet
+  });
+});
 
 
+//trackerFactory.createOne();
 
-
-trackerFactory.createOne();
 // make HTTP requests to API using test client
 // add mark
 // remove mark
