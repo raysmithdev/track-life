01fc1ebdac23032786e027c987ebe0233c0c7765
Annie Chiang <devbear22@gmail.com>
2017-11-22 14:37:25 -0800

    change how controller is imported for cleaner code

--- src/tracker/tracker.router.js
+++ src/tracker/tracker.router.js
@@ -2,37 +2,37 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const { addMark, archiveTracker, createNewTracker, deleteTracker, findArchivedTrackers, findActiveTrackers, findAllTrackers, modifyTrackerDetails, reactivateTracker, removeMark } = require('./tracker.controller');
+const controller = require('./tracker.controller')
 const router = express.Router(); 
 
 router.use(bodyParser.json());
 
 // get all trackers from user
-router.get('/users/:userId/trackers', findAllTrackers)
+router.get('/users/:userId/trackers', controller.findAllTrackers)
 
 // get active trackers from user
-router.get('/users/:userId/trackers/active', findActiveTrackers)
+router.get('/users/:userId/trackers/active', controller.findActiveTrackers)
 
 // get archived trackers from user
-router.get('/users/:userId/trackers/archived', findArchivedTrackers)
+router.get('/users/:userId/trackers/archived', controller.findArchivedTrackers)
 
 // create new tracker
-router.post('/users/:userId/trackers', createNewTracker);
+router.post('/users/:userId/trackers', controller.createNewTracker);
 
 // add mark to an existing tracker (increment by 1)
-router.post('/users/:userId/trackers/:trackerId/increment', addMark)
+router.post('/users/:userId/trackers/:trackerId/increment', controller.addMark)
 
 // remove mark from an existing tracker (decrement by 1)
-router.post('/users/:userId/trackers/:trackerId/decrement', removeMark)
+router.post('/users/:userId/trackers/:trackerId/decrement', controller.removeMark)
 
 // modify tracker details (name, description, notes)
-router.put('/users/:userId/trackers/:trackerId', modifyTrackerDetails)
+router.put('/users/:userId/trackers/:trackerId', controller.modifyTrackerDetails)
 
 // archive tracker (change status code)
-router.post('/users/:userId/trackers/:trackerId/archive', archiveTracker)
+router.post('/users/:userId/trackers/:trackerId/archive', controller.archiveTracker)
 
 // reactivate archived tracker (change status code)
-router.post('/users/:userId/trackers/:trackerId/reactivate', reactivateTracker)
+router.post('/users/:userId/trackers/:trackerId/reactivate', controller.reactivateTracker)
 
 // delete tracker
 // router.post('/users/:userId/trackers/:trackerId/delete', deleteTracker)
