0206e166d54319c5cf36fc527e4580167df9e5db
Annie Chiang <devbear22@gmail.com>
2017-11-21 16:40:34 -0800

    initial setup for test - start/stop server

--- test/test-tracker.js
+++ test/test-tracker.js
@@ -5,21 +5,45 @@ const should = chai.should();
 const faker = require('faker');
 
 const mongoose = require('mongoose');
-const { runServer, closeServer } = require('../server');
+const { app, runServer, closeServer } = require('../server');
 const { TEST_DATABASE_URL } = require('../config');
 const trackerFactory = require('../test/factories/tracker.factory');
 
-//steps to run test
-// connect to database 
-// seed fake data to database - trackers
+function tearDownDb() {
+  return new Promise ((resolve, reject) {
+    console.warn('deleting test database');
+    mongoose.connection.dropDatabase()
+      .then(result => resolve(result))
+      .catch(err => reject(err))
+  });
+}
+
 describe('trackers', function() {
   //let mockUser; 
-  before(function() {
 
-  })
-  beforeEach // only when you want a new set of data
-  after
-  afterEach //
+  // start the server before test
+  before(function() {
+    return runServer(TEST_DATABASE_URL);
+  });
+
+  // close the server after the test is done
+  after(function() {
+    return closeServer();
+  });
+
+  beforeEach(function() {
+    //create trackers
+    return trackerFactory.createMany();
+  }); 
+
+  afterEach(function () {
+    //remove db after each test
+    return tearDownDb();
+  });
+
+  //descirbe is a test case 
+  //it is one test case 
+
   describe('trackers-endpoints', function() {
     
     })
