83cdc3b4d8e58e1cbf1376c46b873b3b91aef4bd
Annie Chiang <devbear22@gmail.com>
2017-11-22 17:07:51 -0800

    add new tracker const

--- test/factories/tracker.factory.js
+++ test/factories/tracker.factory.js
@@ -2,9 +2,21 @@ const faker = require("faker");
 const { Tracker } = require("../../src/tracker/tracker.model");
 const moment = require("moment");
 
-function createOne(userId) {
-  let tallyMarks = {};
-  for (i = 0; i < 6; i++) {
+
+const newTracker = {
+  userId: 123,  //replace?
+  name: faker.commerce.productName(),
+  description: faker.company.catchPhraseDescriptor(),
+  // status: faker.random.number({ min: 1, max: 2 }),
+  notes: faker.company.catchPhrase(),
+  createdDate: new Date(),
+  // tallyMarks: tallyMarksObj
+};
+
+//pass in userId?
+function createOneExisting() {
+  let tallyMarksObj = {};
+  for (let i = 0; i < 6; i++) {
     //get 6 random dates
     let date = faker.date.between(
       moment().subtract(faker.random.number({ min: 1, max: 7 }), "months"),
@@ -13,7 +25,7 @@ function createOne(userId) {
     //reassigning to date as formatted
     date = moment(date).format("YYYY-MM-01");
     //randomly assign tally marks between 1-25
-    tallyMarks[date] = faker.random.number({ min: 1, max: 25 });
+    tallyMarksObj[date] = faker.random.number({ min: 1, max: 25 });
   }
 
   return {
@@ -24,17 +36,19 @@ function createOne(userId) {
     status: faker.random.number({ min: 1, max: 2 }),
     notes: faker.company.catchPhrase(),
     createdDate: new Date(),
-    tallyMarks
+    tallyMarks: tallyMarksObj
   };
 }
 
 function createMany(num) {
   let trackers = [];
-  for (i = 0; i < num; i++) {
-    trackers.push(createOne());
+  for (let i = 0; i < num; i++) {
+    trackers.push(createOneExisting());
   }
   return trackers;
 }
+
+
 // can load this file to see if it works individually
 // console.log(createOne());
-module.exports = { createOne, createMany };
+module.exports = { createOneExisting, createMany, newTracker };
