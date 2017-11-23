7ecbe08dbffa3f877fef14a2a0f700c4d0583ab8
Annie Chiang <devbear22@gmail.com>
2017-11-22 18:48:00 -0800

    add handler for authentication when signing up new account

--- src/auth/auth.routes.js
+++ src/auth/auth.routes.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const passport = require('passport');
-const jwt = require('jsonwebtoken');
-const bodyParser = require('body-parser');
+const express = require("express");
+const passport = require("passport");
+const jwt = require("jsonwebtoken");
+const bodyParser = require("body-parser");
 
-const { User } = require('../user/user.model');
-const config = require('../../config');
+const { User } = require("../user/user.model");
+const config = require("../../config");
 const router = express.Router();
 
 router.use(bodyParser.json());
@@ -14,45 +14,72 @@ const createAuthToken = user => {
   return jwt.sign({ user }, config.JWT_SECRET, {
     subject: user.username,
     expiresIn: config.JWT_EXPIRY,
-    algorithm: 'HS256'
+    algorithm: "HS256"
   });
 };
 
-//check for username and password match
-router.post('/login', (req, res) => {
+// sign up authentication 
+router.post("/signup", async (req, res) => {
+  const userName = req.body.userName;
+  const password = req.body.password;
+
+  const hashedPassword = await User.hashPassword(password);
+
+  const existingUser = await User.findOne({
+    userName
+  });
+
+  if (existingUser) {
+    return res.status(400).json({
+      message: "User already exists"
+    });
+  }
+
+  const newUser = await User.create({
+    userName,
+    password: hashedPassword
+  });
+
+  return res.status(200).json({
+    user: newUser.toClient()
+  })
+});
+
+//check if username and password match
+router.post("/login", (req, res) => {
   const userName = req.body.userName;
   const userPassword = req.body.userPassword;
   User.findOne({ userName: userName })
-    .then(async (user) => {
+    .then(async user => {
       if (!user) {
         return Promise.reject({
-          reason: 'LoginError',
-          message: 'Incorrect username or password'
+          reason: "LoginError",
+          message: "Incorrect username or password"
         });
       }
       return {
         user,
         isValid: await user.validatePassword(userPassword)
       };
-    }) //if user exists, check password 
+    }) //if user exists, check password
     .then(result => {
       //console.log(result);
       if (!result.isValid) {
         return Promise.reject({
-          reason: 'LoginError',
-          message: 'Incorrect username or password'
+          reason: "LoginError",
+          message: "Incorrect username or password"
         });
       }
-      //if password correct, create auth token 
+      //if password correct, create auth token
       const authToken = createAuthToken(result.user.toClient());
       res.status(200).json({
-        meta: 'success',
+        meta: "success",
         userId: result.user._id,
         authToken
       });
     })
     .catch(err => {
-      if (err.reason === 'LoginError') {
+      if (err.reason === "LoginError") {
         return res.status(401).json({ message: err.message });
       }
       console.log(err.stack);
@@ -61,8 +88,8 @@ router.post('/login', (req, res) => {
 });
 
 router.post(
-  '/refresh',
-  passport.authenticate('jwt', { session: false }),
+  "/refresh",
+  passport.authenticate("jwt", { session: false }),
   (req, res) => {
     const authToken = createAuthToken(req.user);
     res.json({ authToken });
