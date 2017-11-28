befc1c99092581b74106f6b83be4e469348b1e8a
Annie Chiang <devbear22@gmail.com>
2017-11-27 20:51:17 -0800

    comment out keys that are not being used

--- src/user/user.model.js
+++ src/user/user.model.js
@@ -3,12 +3,13 @@ const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema ({
-  firstName: String,
-  lastName: String,
+
   userName: {type: String, required: true},
   password: {type: String, required: true},
-  avatar: String,
   trackerIds: [mongoose.Schema.Types.ObjectId]
+  // avatar: String,
+  // firstName: String,
+  // lastName: String,
 });
 
 
@@ -16,16 +17,15 @@ userSchema.virtual('fullName').get(function() {
   return `${this.firstName} ${this.lastName}`.trim();
 });
 
-//what is the purpose of this method?
 userSchema.methods.toClient = function () {
   return {
-    id: this._id,
-    firstName: this.firstName,
-    lastName: this.lastName,
-    fullName: this.fullName,
+    id: this._id,   
     userName: this.userName,
-    avatar: this.avatar,
     trackerIds: this.trackerIds
+    // firstName: this.firstName,
+    // lastName: this.lastName,
+    // fullName: this.fullName,
+    // avatar: this.avatar,
   };
 }
 
