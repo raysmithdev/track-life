680f76240d476435685d1ec3c5856290b462b107
Annie Chiang <devbear22@gmail.com>
2017-12-03 10:31:02 -0800

    change to authentication to 3 days

--- config.js
+++ config.js
@@ -7,6 +7,6 @@ const TEST_DATABASE_URL = process.env.TEST_DATABASE_URL ||
 
 const PORT = process.env.PORT || 8080;
 const JWT_SECRET = 'trackyolife';
-const JWT_EXPIRY = process.env.JWT_EXPIRY || '1d';
+const JWT_EXPIRY = process.env.JWT_EXPIRY || '3d';
 
 module.exports = {DATABASE_URL, JWT_EXPIRY, JWT_SECRET, PORT, TEST_DATABASE_URL};
